d6ee3532a3c5ebadc16ac4e778b4ed0e8eb70d3c
EvanAdhiguna <147408852+EvanAdhiguna@users.noreply.github.com>
2024-05-01 00:52:34 +0800

    Update Order.java
    mengkoreksi kesalahan

--- Order.java
+++ Order.java
@@ -1,31 +1,54 @@
 package TUGAS_PBO;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 
 public class Order {
-    private static Scanner scanner = new Scanner(System.in);
 
-    public static void menuOrder() {
-        System.out.println("=========== Menu Order ==========");
-        System.out.println("1. Lihat Menu");
-        System.out.println("2. Logout");
-        System.out.println("===============================");
+    private Restaurant restaurant;
+    private HashMap<Menu, Integer> menus;
+    private int distance;
+
+    public Order(Restaurant restaurant) {
+        this.restaurant = restaurant;
+        this.menus = new HashMap<>();
     }
 
-    public static void viewMenu() {
-        System.out.println("=========== Menu Restoran ==========");
-        System.out.println("1. Pizza Margherita");
-        System.out.println("2. Pizza Pepperoni");
-        System.out.println("3. Pizza Hawaiian");
-        System.out.println("4. Pizza Supreme");
-        System.out.println("5. Pizza Vegetarian");
-        System.out.println("6. Pizza BBQ Chicken");
-        System.out.println("7. Pizza Meat Lovers");
-        System.out.println("8. Pizza Four Cheese");
-        System.out.println("9. Pizza Seafood");
-        System.out.println("10. Pizza Capricciosa");
-        System.out.println("===================================");     
-        scanner.close();   
+    public Restaurant getRestaurant() {
+        return this.restaurant;
     }
+
+    public HashMap<Menu, Integer> getMenus() {
+        return this.menus;
+    }
+
+    public int getDistance() {
+        return this.distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (Menu menu : this.menus.keySet()) {
+            totalPrice += menu.getHarga() * this.menus.get(menu);
+        }
+        totalPrice += this.distance * 1000;
+        return totalPrice;
+    }
+
+    public void addMenu(Menu menu, int quantity) {
+        if (this.menus.containsKey(menu)) {
+            int currentQuantity = this.menus.get(menu);
+            this.menus.put(menu, currentQuantity + quantity);
+        } else {
+            this.menus.put(menu, quantity);
+        }
+    }
+
 
 }
+
